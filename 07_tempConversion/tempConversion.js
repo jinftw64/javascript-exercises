30aa65806928f029e5cfa94f0318c50d57c21c03
jinftw64 <jin@PLATCUBAHILL16>
2023-03-13 16:15:23 -0400

    Complete tempConversion.js and pass all 6 unit tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(int) {
+  return Math.round((int - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(int) {
+  return Math.round((int * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
