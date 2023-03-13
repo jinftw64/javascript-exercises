c41676466774ab2ba9bb99e69cddef61b0cebf81
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-03-12 23:09:53 -0400

    Complete reverseString.js and pass all 4 unit tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let numChars = string.length;
+  let newString = '';
+  let letter;
+  for (let i = 0; i < numChars; i++) {
+    letter = string.charAt(numChars - 1 - i);
+    newString += letter;
+  }
+  return newString;
 };
 
 // Do not edit below this line
