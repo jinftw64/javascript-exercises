754751a6efaaca6faabd0500d7cae9eefc8ad8ac
jinftw64 <jin@PLATCUBAHILL16>
2023-03-13 15:38:32 -0400

    Complete sumAll.js and pass all 6 unit tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  const isPositiveInt = int => {
+    return Number.isInteger(int) && int > 0;
+  }
+  if (!isPositiveInt(a) || !isPositiveInt(b)) {
+    return 'ERROR'
+  }
+  firstInt = Math.min(a, b);
+  secondInt = Math.max(a, b);
+  var sum = 0;
+  for (let i = firstInt; i <= secondInt; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
