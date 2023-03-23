22d5447a7eda68001efd713c251e8277b4266f2d
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-03-22 22:37:45 -0400

    Create factorial function with recursion and pass all 5 unit tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,9 @@ const power = function(a, b) {
   return a ** b
 };
 
-const factorial = function() {
-
+const factorial = function(a) {
+  if (a === 0 || a === 1) return 1;
+  return a * factorial(a - 1);
 };
 
 // Do not edit below this line
