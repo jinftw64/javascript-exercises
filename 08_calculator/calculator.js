95b677c095ef85eb215481a3377ff4453be9c9ca
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-03-22 22:29:00 -0400

    Create power function and pass the unit test.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,8 +14,8 @@ const multiply = function(array) {
   return array.reduce((previous, current) => previous * current)
 };
 
-const power = function() {
-
+const power = function(a, b) {
+  return a ** b
 };
 
 const factorial = function() {
