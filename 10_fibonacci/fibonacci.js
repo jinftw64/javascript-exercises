1666498066a97b9d7fbae13e0c8f127e97a3ad2b
jinftw46 <jin@PLATCUBAHILL16>
2023-03-23 10:39:15 -0400

    Create fibonacci function with recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  const int = parseInt(num);
+  if (int < 1) return 'OOPS';
+  if (int === 1 || int === 2) return 1;
+  return fibonacci(int - 1) + fibonacci(int - 2);
 };
 
 // Do not edit below this line
