6879cdca6fcb696d4e3112b2e6b1f4cbc44e8782
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-03-22 23:37:10 -0400

    Create palindrome detector function and pass all 6 unit tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+  const removeSpaces = string.toLowerCase().replace(/[^a-z]/g, '');
+  return removeSpaces == removeSpaces.split('').reverse().join('');
 };
 
 // Do not edit below this line
