f0a3d659a9ea781cd31138787c4a3862404c887f
jinftw46 <jin@PLATCUBAHILL16>
2023-03-23 17:12:31 -0400

    Create findOldest function with reduce method and returning comparison expression with ternary operator to with the person object.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const ageOldestPerson = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    const ageCurrentPerson = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    return ageOldestPerson < ageCurrentPerson ? currentPerson : oldestPerson;
+  })
 };
 
 // Do not edit below this line
